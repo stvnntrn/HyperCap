d9799769e6033991ce538f3298ddc83088c66bbb
Steven Tran <123steventran@hotmail.se>
2025-03-06 21:09:04 +0100

    Styling on the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,32 @@
 import { Link } from "react-router-dom";
-import { FaSearch, FaBriefcase, FaStar } from "react-icons/fa"; // Import icons
+import { FaSearch, FaBriefcase, FaStar } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <nav className="bg-white text-gray-900 py-4 w-full border-b border-[#EFF2F5]">
       <div className="container mx-auto flex justify-between items-center px-6">
         <div className="flex items-center space-x-8">
-          <Link
-            to="/"
-            className="text-2xl font-bold hover:text-gray-600 mb-1.5"
-          >
+          <Link to="/" className="text-2xl font-bold hover:text-primary mb-1.5">
             HyperCap
           </Link>
           <ul className="flex space-x-8 font-semibold">
             <li>
-              <Link to="/compare" className="hover:text-gray-600">
+              <Link to="/compare" className="hover:text-primary">
                 Compare
               </Link>
             </li>
             <li>
-              <Link to="/roi-calculator" className="hover:text-gray-600">
+              <Link to="/roi-calculator" className="hover:text-primary">
                 ROI Calculator
               </Link>
             </li>
             <li>
-              <Link to="/staking-calculator" className="hover:text-gray-600">
+              <Link to="/staking-calculator" className="hover:text-primary">
                 Staking Calculator
               </Link>
             </li>
             <li>
-              <Link to="/learn" className="hover:text-gray-600">
+              <Link to="/learn" className="hover:text-primary">
                 Learn
               </Link>
             </li>
@@ -41,16 +38,16 @@ const Navbar = () => {
             <li>
               <Link
                 to="/portfolio"
-                className="flex items-center space-x-2 hover:text-gray-600"
+                className="flex items-center space-x-2 hover:text-primary"
               >
-                <FaBriefcase className="text-gray-600" />
+                <FaBriefcase className="text-[#88501c]" />
                 <span>Portfolio</span>
               </Link>
             </li>
             <li>
               <Link
                 to="/watchlist"
-                className="flex items-center space-x-2 hover:text-gray-600"
+                className="flex items-center space-x-2 hover:text-primary"
               >
                 <FaStar className="text-yellow-500" />
                 <span>Watchlist</span>
@@ -62,12 +59,12 @@ const Navbar = () => {
             <input
               type="text"
               placeholder="Search..."
-              className="bg-gray-200 text-gray-900 pl-10 pr-3 py-1.5 rounded-md w-44 text-sm focus:outline-none focus:ring-2 focus:ring-gray-500"
+              className="bg-gray-200 text-gray-900 pl-10 pr-3 py-1.5 rounded-md w-44 text-sm focus:outline-none focus:ring-2 focus:ring-primary"
             />
             <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-600" />
           </div>
 
-          <button className="bg-blue-600 text-white px-4 py-1.5 rounded-md text-sm font-semibold hover:bg-blue-500">
+          <button className="bg-primary hover:bg-primary text-white px-4 py-1.5 rounded-md text-sm font-semibold cursor-pointer transition-all duration-300 ease-in-out transform hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-primary">
             Log In
           </button>
         </div>
